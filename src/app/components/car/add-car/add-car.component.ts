eb8702ce63c72762c73a61c68ab4aab42d993289
Marius <marius.gravningsmyhr@gmail.com>
2022-11-13 20:36:17 +0100

    Add navbar

---
+++ src/app/components/car/add-car/add-car.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Car } from 'src/app/models/car';
+import { CarService } from 'src/app/services/car.service';
+
+@Component({
+  selector: 'app-add-car',
+  templateUrl: './add-car.component.html',
+  styleUrls: ['./add-car.component.css'],
+})
+export class AddCarComponent implements OnInit {
+  car: Car = {
+    make: '',
+    model: '',
+    hp: 0,
+    imageUrl: '',
+  };
+  constructor(private carService: CarService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  addCar() {
+    this.carService.addCar(this.car).subscribe({
+      next: (car) => {
+        this.car = car;
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+}
